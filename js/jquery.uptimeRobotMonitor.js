deb1c13bf7dd5985e6f61c495aca261a5d2da126
dipdiri <dip.diri@yahoo.com>
2014-04-14 04:57:50 +0300

    Update jquery.uptimeRobotMonitor.js

--- js/jquery.uptimeRobotMonitor.js
+++ js/jquery.uptimeRobotMonitor.js
@@ -349,7 +349,7 @@
 			drawTextAlongArc(c, text, xMid, yMid, radius, 0.7*Math.PI);
 		}else if(monitorConf.isStatus){
 			var statusText = perc == 0.01 ? "offline" : "online";
-			c.fillText(statusText, xMid+20, yMid+2);
+			c.fillText(statusText, xMid+25, yMid+3);
 		}
 	}
 	
