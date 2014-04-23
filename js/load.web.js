66698f3ae168602ff3def293f8546a084a9c08a1
dipdiri <dip.diri@yahoo.com>
2014-04-23 19:12:13 +0300

    Update load.web.js

--- js/load.web.js
+++ js/load.web.js
@@ -88,7 +88,4 @@ document.body.onload = addElement;
 function addElement () { 
 	var theDiv = document.getElementById("one");
 	theDiv.innerHTML += "<p>（。 ㅅ 。）<small>dipdiri.com</small></p><dl><dd><small>- fotoğraf gönderme kuralları!</small></dd><dd><small>-- bacaklar <span class='s'>görünmeyecek</span></small></dd><dd><small>-- <span class='nc'>dipdiri</span> memeler <span class='nc'>gürünecek</span></small></dd></dl>"; 
-} 
-
-
-$('.cropme').simpleCropper();
+}
