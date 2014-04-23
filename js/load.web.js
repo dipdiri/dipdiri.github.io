20ac31dd511cdbf20a3bd298a0f1ae2c3b4597ff
dipdiri <dip.diri@yahoo.com>
2014-04-23 18:57:41 +0300

    Update load.web.js

--- js/load.web.js
+++ js/load.web.js
@@ -66,7 +66,9 @@ loadScripts([
 	"http://code.jquery.com/jquery-1.11.0.min.js",
 	"//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js",
  	"http://jasny.github.io/bootstrap/dist/js/jasny-bootstrap.min.js",
-	"http://lokeshdhakar.com/projects/lightbox2/js/lightbox.js"
+	"http://lokeshdhakar.com/projects/lightbox2/js/lightbox.js",
+	"//cdnjs.cloudflare.com/ajax/libs/holder/2.3.1/holder.js",
+        "//cdnjs.cloudflare.com/ajax/libs/jquery-jcrop/0.9.12/js/jquery.Jcrop.min.js"
 ],function(){
     console.info('loaded.');
 });
