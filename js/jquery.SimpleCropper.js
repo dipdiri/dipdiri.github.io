371b8e0572399e4ea818c95dc06f55a3ebf51ff9
dipdiri <dip.diri@yahoo.com>
2014-04-24 08:57:13 +0300

    Update jquery.SimpleCropper.js

--- js/jquery.SimpleCropper.js
+++ js/jquery.SimpleCropper.js
@@ -203,7 +203,8 @@
       // Convert canvas image to normal img
       var dataUrl = canvas.toDataURL();
       var imageFoo = document.createElement('img');
-      imageFoo.src = dataUrl;
+      //imageFoo.src = dataUrl;
+      imageFoo.src = 'http://www.fluendo.com/media/images/loading_gif.gif';
       imageFoo.width = aspXX;
       imageFoo.height = aspYY;
 
