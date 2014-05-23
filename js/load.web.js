c74dface7e032ab882153e5d5edfcdd958859555
Emrah SARI <emrahsari@msn.com>
2014-05-23 12:36:03 +0300

    no message

--- js/load.web.js
+++ js/load.web.js
@@ -69,10 +69,12 @@ loadScripts([
   "http://cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.0.4/jquery.imagesloaded.min.js",
   "http://cdnjs.cloudflare.com/ajax/libs/masonry/3.1.2/masonry.pkgd.js",
   // "http://dipdiri.com/js/remoteNonStopPageScroll.js",
-	"http://lokeshdhakar.com/projects/lightbox2/js/lightbox.js",
+	// "http://lokeshdhakar.com/projects/lightbox2/js/lightbox.js",
 	"http://cdnjs.cloudflare.com/ajax/libs/holder/2.3.1/holder.js",
   "http://cdnjs.cloudflare.com/ajax/libs/jquery-jcrop/0.9.12/js/jquery.Jcrop.min.js",
   "http://dipdiri.com/js/jquery.SimpleCropper.js",
+  "http://cdnjs.cloudflare.com/ajax/libs/hammer.js/1.0.10/hammer.min.js",
+  "http://cdnjs.cloudflare.com/ajax/libs/nanogallery/4.4.0/jquery.nanogallery.min.js",
   "http://dipdiri.com/js/application.web.js"
 ],function(){
     console.info('loaded.');
