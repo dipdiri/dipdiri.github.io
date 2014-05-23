c328e28da93bee0efcf96be77e708932c1f54acb
Emrah SARI <emrahsari@msn.com>
2014-05-23 13:07:16 +0300

    no message

--- js/application.web.js
+++ js/application.web.js
@@ -1,9 +1,6 @@
 $(function() {
-	$('a').fullsizableDynamic({
-		detach_id: 'container',
-		navigation: true,
-		closeButton: true,
-		clickBehaviour: 'next'
+	$.fullsizableDynamic('#four a', {
+  		navigation: true
 	});
 });
 
