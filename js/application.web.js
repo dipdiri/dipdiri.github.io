f88cedfcad47a20f83b57c0f1f3a9b544a7ef2f3
Emrah SARI <emrahsari@msn.com>
2014-05-24 12:13:25 +0300

    no message

--- js/application.web.js
+++ js/application.web.js
@@ -9,11 +9,11 @@
 
 // $('.cropme').simpleCropper();
 
-// var $contents = $('#contents');
-// $contents.imagesLoaded( function() {
-//     $contents.masonry({
-// 	  	itemSelector: '.item',
-// 		isFitWidth: true,
-// 		stamp: '.stamp'
-// 	});
-// });
+var $contents = $('#four-tag');
+$contents.imagesLoaded( function() {
+    $contents.masonry({
+	  	itemSelector: '.item',
+		isFitWidth: true,
+		stamp: '.stamp'
+	});
+});
